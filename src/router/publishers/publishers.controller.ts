8dbd302a32ad5be25aacce370079da8a4f4a0add
Tim Goshinski <tim@code-chimp.com>
2024-02-06 06:54:46 -0600

    :sparkles: Publishers update

---
+++ src/router/publishers/publishers.controller.ts
@@ -0,0 +1,19 @@
+export const getAll = async (req, res) => {
+  res.status(404).json({ message: 'Not implemented' });
+};
+
+export const getOne = async (req, res) => {
+  res.status(404).json({ message: 'Not implemented' });
+};
+
+export const createEntity = async (req, res) => {
+  res.status(404).json({ message: 'Not implemented' });
+};
+
+export const updateEntity = async (req, res) => {
+  res.status(404).json({ message: 'Not implemented' });
+};
+
+export const removeEntity = async (req, res) => {
+  res.status(404).json({ message: 'Not implemented' });
+};
